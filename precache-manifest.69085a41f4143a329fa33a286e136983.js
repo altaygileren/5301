0748a78e950b00c21c50439d2dae3f082af1f588
altaygileren <erenaltaygil@gmail.com>
2019-10-19 15:53:20 -0400

    Updates

---
+++ precache-manifest.69085a41f4143a329fa33a286e136983.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa2f4b046b6bfa8386754558e986565",
+    "url": "/5301/index.html"
+  },
+  {
+    "revision": "492bd04492f48a352f92",
+    "url": "/5301/static/css/main.12610158.chunk.css"
+  },
+  {
+    "revision": "2462a6001bd211bae079",
+    "url": "/5301/static/js/2.a55376b0.chunk.js"
+  },
+  {
+    "revision": "492bd04492f48a352f92",
+    "url": "/5301/static/js/main.745ba2ab.chunk.js"
+  },
+  {
+    "revision": "0b64795760712a025591",
+    "url": "/5301/static/js/runtime-main.26c760d4.js"
+  },
+  {
+    "revision": "6c6c5ca2865fad530f1b4ef5091d4d5a",
+    "url": "/5301/static/media/floor1.6c6c5ca2.jpg"
+  },
+  {
+    "revision": "4a2e49448a97c7da8ecd5d7432b25d19",
+    "url": "/5301/static/media/floor2.4a2e4944.jpg"
+  },
+  {
+    "revision": "7340811371bb513f90e9b0848da196c0",
+    "url": "/5301/static/media/floor3.73408113.jpg"
+  },
+  {
+    "revision": "248224e581aca29c9fd08503e9d6a7d2",
+    "url": "/5301/static/media/floor4.248224e5.jpg"
+  },
+  {
+    "revision": "7fce991b8d264ad2a6d9d54a3d3199d3",
+    "url": "/5301/static/media/floorplan.7fce991b.png"
+  },
+  {
+    "revision": "a385810632461986833e9d5140679bd1",
+    "url": "/5301/static/media/heroimg.a3858106.png"
+  },
+  {
+    "revision": "44ddcac09617aa4a4fe8e319a7c4e795",
+    "url": "/5301/static/media/kitchen.44ddcac0.jpg"
+  },
+  {
+    "revision": "b53e5e1222655f100a9bb6d2ffc92bf2",
+    "url": "/5301/static/media/parallaximg2.b53e5e12.png"
+  },
+  {
+    "revision": "ad287a59f230d028b0d059da74a0a192",
+    "url": "/5301/static/media/parallaximg3.ad287a59.png"
+  }
+]);
